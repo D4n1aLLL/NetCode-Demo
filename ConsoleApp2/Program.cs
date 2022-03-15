b49acd184adda467b40874bccfbdd74f5df15d5e
Danial Ahmed <>
2022-03-15 12:42:17 +0500

    Init

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.IO;
+using NetBarcode;
+using Type = NetBarcode.Type;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            for (int i = 0; i < 5000; i++)
+            {
+                Barcode bar = new Barcode("123456789123456", Type.Code128);
+                using (var image = bar.GetImage())
+                {
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        image.Save(ms,ImageFormat.Png);
+                        var base64 = Convert.ToBase64String(ms.ToArray());
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            Console.ReadKey();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ dockerfile
@@ -0,0 +1,23 @@
+#See https://aka.ms/containerfastmode to understand how Visual Studio uses this Dockerfile to build your images for faster debugging.
+
+FROM mcr.microsoft.com/dotnet/aspnet:5.0.15-focal as base
+
+COPY ["./dotMemory/", "/app/"]
+RUN apt-get update && apt-get install -y libc6-dev && apt-get install -y libgdiplus
+WORKDIR /app
+
+FROM mcr.microsoft.com/dotnet/sdk:5.0.406-focal AS build
+WORKDIR /src
+COPY ["ConsoleApp2/ConsoleApp2.csproj", "ConsoleApp2/"]
+RUN dotnet restore "ConsoleApp2/ConsoleApp2.csproj"
+COPY . .
+WORKDIR "/src/ConsoleApp2"
+RUN dotnet build "ConsoleApp2.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "ConsoleApp2.csproj" -c Release -o /app/publish
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "ConsoleApp2.dll"]
